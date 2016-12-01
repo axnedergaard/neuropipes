924b6da4d65268099cf2099549a18862814640fc
axnedergaard <axnedergaard@gmail.com>
2016-12-01 23:21:25 +0000

    Add random offset to pipe_dummyemotivconcurrent to detect concurrency problems.

--- src/pipes/pipe_dummyemotivconcurrent.c
+++ src/pipes/pipe_dummyemotivconcurrent.c
@@ -20,15 +20,15 @@ int dummyemotivconcurrent_init(pipe_* p, linkedlist* l)  {
 
 int dummyemotivconcurrent_run(pipe_* p, linkedlist* l)  {
   srand(time(NULL));
+  int offset = rand() % 100;  //random
+
   double *buffer = (double*)p->auxiliary;
   for (int i = 0; i < p->output->shape[0]; i++)  {
     for (int j = 0; j < p->output->shape[1]; j++)  {
-      buffer[i*p->output->shape[1] + j] = i*p->output->shape[1] + j;
+      buffer[i*p->output->shape[1] + j] = i*p->output->shape[1] + j + offset;
     }
   }
-  //mutex lock TODO (if concurrent)
   data_copy_to_data(p->output, buffer);
-  //mutex unlock
   return 1; 
 }
 
